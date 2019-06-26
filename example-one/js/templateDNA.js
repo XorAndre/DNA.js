70d7de2bfcbce02f872996e1bbfde63f96bfe509
XorAndre <xor2017@outlook.com>
2019-06-26 12:58:23 -0300

    Exemplo com paragrafo

--- example-one/js/templateDNA.js
+++ example-one/js/templateDNA.js
@@ -1,5 +1,5 @@
 const cards =[
-    {title: 'Artigo-um', author: 'Andre Laurentino'},
+    {title: 'Artigo-um', post: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.',  author: 'Andre Laurentino'},
     {title: 'Artigo-dois', author: 'Andre Laurentino'},
     {title: 'Artigo-tres', author: 'Andre Laurentino '},
 ]; 

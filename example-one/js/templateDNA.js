07de25c184c411230c574d3417cb385bb7ec1384
XorAndre <xor2017@outlook.com>
2019-06-26 12:55:07 -0300

    Update exemplo um

--- example-one/js/templateDNA.js
+++ example-one/js/templateDNA.js
@@ -1,2 +1,7 @@
-const objeto = {title: 'Objeto DOM Criado', author: 'Andre Laurentino' }; 
-dna.clone('objeto', objeto);
+const cards =[
+    {title: 'Artigo-um', author: 'Andre Laurentino'},
+    {title: 'Artigo-dois', author: 'Andre Laurentino'},
+    {title: 'Artigo-tres', author: 'Andre Laurentino '},
+]; 
+dna.clone('cards', cards);
+//Create DOM elemento

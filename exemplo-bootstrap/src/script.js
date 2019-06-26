638548e9794439853f28ef0e3f9a41f003f03644
XorAndre <xor2017@outlook.com>
2019-06-26 18:49:18 -0300

    Atualização de exemplos

---
+++ exemplo-bootstrap/src/script.js
@@ -0,0 +1,14 @@
+//Menu
+let itens = [
+    {link: 'Home', endereco: 'index.html'},
+    {link: 'Sobre-Dev', endereco: 'dev.html'},
+    {link: 'Serviços', endereco: 'servicos.html'},
+    {link: 'Portfólio', endereco: 'portfolio.html'},
+    {link: 'Contato', endereco: 'contato.html'},
+    {link: 'Blog', endereco: 'blog.html'},
+]; 
+//Textos sections
+let theDev = {about: 'Um pouco sobre mim', about_text: 'Muito prazer, meu nome é André Laurentino Rodrigues tenho 25 anos de idade sou nascido em Luz pequena cidade situada no estado de Minas Gerais. Sou formado em técnico de informática e análise de sistemas. Durante muito tempo trabalhei como técnico em suporte de computadores até descobrir minha real habilidade em desenvolvimento de sistemas para web, já ministrei cursos como WebNews no IFMG-Bambuí(Instituto Federal de Minas Gerais em Bambuí)e também trabalhei como professor de web na Escola Estadual Comendador Zico Tobias. Hoje sou engenheiro Front-End e contribuo no portal GSTI Brasil com artigos voltados para desenvolvimentos de sistemas web, mas meus conhecimentos não se limitam apenas em códigos também tenho domínio em design gráfico(pacotes de programas Adobe, Linux Interfaces gráficas e CorelDraw)e SEO. Meus passatempos são a leitura, jogos(nas horas vagas), assistir filmes e escrever códigos.', eu: 'https://scontent.fudi1-1.fna.fbcdn.net/v/t1.0-9/35439077_223962991666477_5774740312958173184_n.jpg?_nc_cat=103&_nc_eui2=AeEuoo84CVEetMkwKr8o3j1F9O71ZnzCrsT2BbknXLxcZDB3x9keHEGM7Y99g3hWAiWR4nE0Xl5YAzAHBs-jzG9iXH1BdT2ErC1BGLeGsckWfA&_nc_oc=AQkk6uQQGKUgmYBBGB8BGfeespxigbLh7WitHPX2S3tPzXyMqOYdoyVDn7CDZpOn46o&_nc_ht=scontent.fudi1-1.fna&oh=1841909ad44063bec2da5d55d3992319&oe=5DBD43AB'};
+//return itens
+dna.clone('itens', itens);
+dna.clone('theDev', theDev);
